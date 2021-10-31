9252ea937ff54c4d053462938bf3e294bece0585
sheng <86102390+sheng120777@users.noreply.github.com>
2021-10-31 23:54:17 +0800

    Create 05704.cpp

---
+++ 5700-5799/05704.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long
+#define endl "\n"
+#define inf 1e9
+#define maxn 10000005
+
+signed main(){
+
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    char c;
+    cin>>c;
+    cout<<char(c-32);
+}
