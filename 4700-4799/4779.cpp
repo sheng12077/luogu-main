4637e443b84aa0e95926c617f299c2fe54aa6c1f
sheng <86102390+sheng120777@users.noreply.github.com>
2021-11-02 22:02:50 +0800

    Update 4779.cpp

--- 4700-4799/4779.cpp
+++ 4700-4799/4779.cpp
@@ -21,33 +21,33 @@ signed main(){
 
     cin>>n>>m>>start;
 
-    memset(dis,0x3F,sizeof(dis));
+    memset(dis,0x3f,sizeof(dis));
     dis[start]=0;
-    
+
     for(int i=1;i<=m;i++){
         cin>>a>>b>>w;
         graph[a].push_back({b,w});
     }
 
-    for(int i=1;i<=n;i++){
-        pq.push({0,start});
-        while(!pq.empty()){
-            auto now=pq.top();
-            pq.pop();
-            int v=now.second;
-            if(visited[v]){
-                continue;                   
-            }
-            visited[v]=true;                   
-            for(auto next:graph[v]){         
-                int u=next.first;            
-                int k=next.second;           
-                if(dis[u]>dis[v]+k){
-                    dis[u]=dis[v]+k;
-                    pq.push({dis[u],u});
-                }
+    pq.push({0,start});
+    while(!pq.empty()){
+        auto now=pq.top();
+        pq.pop();
+        int v=now.second;
+        if(visited[v]){
+            continue;
+        }       
+        visited[v]=true;
+        for(auto next:graph[v]){
+            int u=next.first;
+            int k=next.second;
+            if(dis[u]>dis[v]+k){
+                dis[u]=dis[v]+k;
+                pq.push({dis[u],u});
             }
         }
+    }
+    for(int i=1;i<=n;i++){
         cout<<dis[i]<<" ";
     }
 }
