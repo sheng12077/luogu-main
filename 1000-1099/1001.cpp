006feaeff27703c68b6a337da4a3264dd447d285
sheng12077 <141042325+sheng12077@users.noreply.github.com>
2023-11-10 23:08:06 +0800

    Create 1001.cpp

---
+++ 1000-1099/1001.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long
+#define endl '\n'
+#define inf 2e18
+#define maxn 100005
+#define mod 1000000007
+
+signed main(){
+
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int a,b;
+    cin>>a>>b;
+    cout<<a+b;
+}
